b656b89513948c18eb2d76e7da1ef34e3155b9e3
Return-name <rishabha649@gmail.com>
2023-03-26 12:48:07 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send("OK");
+  res.json({'message': 'ok'});
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+/* Error handler middleware */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  console.error(err.message, err.stack);
+  res.status(statusCode).json({'message': err.message});
+  
+  return;
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App listening at http://localhost:${port}`)
+});
